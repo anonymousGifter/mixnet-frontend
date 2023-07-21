75ba13ea3847b08577aa0d0479350becf4b4d977
ZakariaR1ad <r.mordi@esi-sba.dz>
2023-07-22 00:56:24 +0100

    adding the fhevm file

---
+++ mixnet-frontend/src/fhevm.js
@@ -0,0 +1,24 @@
+import { BrowserProvider,JsonRpcProvider } from 'ethers';
+import { initFhevm, createInstance } from 'fhevmjs';
+
+export const init = async () => {
+  await initFhevm();
+};
+
+let instance;
+
+export const createFhevmInstance = async () => {
+  const provider = new BrowserProvider(window.ethereum) ;
+//   const provider = new JsonRpcProvider('https://devnet.fhenix.io/');
+  const network = await provider.getNetwork();
+  const chainId = +network.chainId.toString();
+  const publicKey = await provider.call({
+    from: null,
+    to: '0x0000000000000000000000000000000000000044',
+  });
+  instance = await createInstance({ chainId, publicKey });
+};
+
+export const getInstance = () => {
+  return instance;
+};
--- mixnet-frontend/src/pages/withdraw.jsx
+++ mixnet-frontend/src/pages/withdraw.jsx
@@ -1,19 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { Form } from "react-bootstrap";
 import { useState } from "react";
-// import {isAddress} from 'web3-validator';
-// import {isAddress}
-import { Wallet, JsonRpcProvider, Contract } from "ethers";
+import { getInstance,createFhevmInstance } from '../fhevm'
 import {isAddress, toHexString} from '../Utils';
-import { createInstance } from "fhevmjs";
 
 function Withdraw(){
     
-
-
-
     const [amount, setAmount] = useState(0)
     const [address, setAddress] = useState(0)
+    const instance = createFhevmInstance();
 
     const handleAmountChange = (event) => {
         setAmount(event.target.value)
@@ -30,27 +25,7 @@ function Withdraw(){
     const confirm = (e) => {
         e.preventDefault();
 
-        const provider = new JsonRpcProvider("https://devnet.fhenix.io/");
-        const wallet = new Wallet("0x92293977156de6e03b20b26708cb4496b523116190b5c32d77cee8286d0c41f6", provider);
 
-        let _instance;
-
-        const getInstance = async () => {
-            if (_instance) return _instance;
-
-            // 1. Get chain id
-            const network = await provider.getNetwork();
-            const chainId = +network.chainId.toString(); // Need to be a number
-
-            // Get blockchain public key
-            const publicKey = await provider.call({
-                to: "0x0000000000000000000000000000000000000044",
-            });
-
-            // Create instance
-            _instance = createInstance({ chainId, publicKey });
-            return _instance;
-        };
         if(amount <= 0){
             alert("Amount must be greater than 0");
             return
@@ -59,9 +34,8 @@ function Withdraw(){
         //     alert("Not a valid address");
         //     return
         // }
-        const instance = getInstance();
-        const encryptedAmount = instance.encrypt32(amount);
-        console.log("encryptedAmount: ", toHexString(encryptedAmount));
+        console.log("Amount: ", instance);
+        
 
     }
     return(
